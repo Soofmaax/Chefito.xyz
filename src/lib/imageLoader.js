d155636328a433ed2530955053f76a7db11800e4
Salwa <124178747+Soofmaax@users.noreply.github.com>
2025-06-30 15:30:06 +0200

    Update imageLoader.js

--- src/lib/imageLoader.js
+++ src/lib/imageLoader.js
@@ -1,13 +1,11 @@
 // src/lib/imageLoader.js
 function imageLoader({ src, width, quality }) {
   if (!src) return '';
-
   if (src.startsWith('http://') || src.startsWith('https://')) {
     return src;
   }
-
   const q = quality || 75;
   return `${src}?w=${width}&q=${q}`;
 }
 
-module.exports = imageLoader;
+export default imageLoader;
